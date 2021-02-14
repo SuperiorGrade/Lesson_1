eb2b303c997b74b5584722af260d2ae14b01a5a7
SuperiorGrade <SuperiorGrade@list.ru>
2021-02-14 16:25:01 +0300

    add HelloUser project

---
+++ HelloUser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloUser
+{
+    class Program
+    {
+        static string Username;
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            Username = Console.ReadLine();
+            Console.WriteLine($"Привет, {Username}, сегодня {DateTime.Now:d}");
+        }
+    }
+}
